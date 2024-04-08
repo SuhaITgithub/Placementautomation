e6ea18f7a2337dba324bcef40ef0f3e2bd4070f7
SuhaITgithub <148217632+SuhaITgithub@users.noreply.github.com>
2024-04-08 12:05:14 +0530

    Add files via upload

---
+++ csp/about.php
@@ -0,0 +1,72 @@
+<?php
+    include "includes/header.php";
+?>
+		<div class="col-md-9 main" >
+		<!--banner-section-->
+            <div class="banner-section">
+            <!--/top-news-->
+            <div class="top-news">
+                <div class="about-content">
+                    <!-- about -->
+                    <h3 class="tittle">About <i class="glyphicon glyphicon-user"></i></h3>
+                    <img src="image_uploads/logo.png" alt="" />
+                    <p>PLACEMENT RESUME READER  is trusted by over 1 million job seekers worldwide.
+OUR VISION IS TO HELP YOU LAND MORE INTERVIEWS AND JOB OFFERS THAN EVER BEFORE PLACEMENT RESUME READER  is trusted by top higher education
+institutions and leaders in employment outcomes</label></p>
+                    <div class="clearfix"></div>
+                </div-->
+            </div>
+            <!--//top-news-->
+        </div>
+    <!--//banner-section-->
+    <div class="banner-right-text">
+        <h3 class="tittle">Our Team <i class="glyphicon glyphicon-star"></i></h3>
+        <!--/general-news-->
+        <div class="general-news">
+            <div class="general-inner">
+                <div class="edit-pics">
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f4.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a href="single.html">VIMALA DEVI</a></h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a href="single.html">AKSHAYA</a></h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a href="single.html">SABARI BAH </a></h5>
+                            <div class="td-post-date two"> </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--//general-news-->
+        <!--/news-->
+        <!--/news-->
+    </div>
+    </div>
+    <div class="clearfix"> </div>
+<?php
+include "includes/footer.php";
+?>
